3d35d55a18f82480028f7521ec64c4c5ebdfe93c
Indieology <contactindieology@gmail.com>
2022-04-19 20:14:22 -0500

    Fixes player not dying after animation finishes
    Added a queue_free method call inside of the player AnimationPlayer node, instead of trying to call it inside of the death_state script.

--- Player/State Machine/death_state.gd
+++ Player/State Machine/death_state.gd
@@ -5,8 +5,6 @@ func physics_process(delta: float) -> int:
 	player.velocity = Vector2.ZERO
 	get_parent().get_parent().get_node("Networking").sync_velocity = Vector2.ZERO
 	
-	await player.animation_player.animation_finished
-	player.queue_free()
 	
 	return State.Null
 	
